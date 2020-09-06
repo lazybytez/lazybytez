44d9bcc59d7c44635399acb4a0e84a1db69d2607
Elias Knodel <elias.knodel@gmail.com>
2020-09-06 22:06:48 +0200

    Add vue router

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
   <div id="app">
-    <Index/>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
-
-<script lang="ts">
-import { Component, Vue } from 'vue-property-decorator'
-import Index from './components/Index.vue'
-
-@Component({
-  components: {
-    Index
-  }
-})
-export default class App extends Vue {}
-</script>
 
 <style lang="less">
 #app {
@@ -23,6 +15,18 @@ export default class App extends Vue {}
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
