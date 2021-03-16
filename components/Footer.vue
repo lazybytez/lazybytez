9f22c1a674c968d492b3618fc7b75676b191f337
Elias Knodel <elias.knodel@gmail.com>
2021-03-17 00:18:42 +0100

    Fix failing tests

--- components/Footer.vue
+++ components/Footer.vue
@@ -19,13 +19,3 @@
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
-
-<style lang="less" scoped>
-
-</style>
--- pages/impressum.vue
+++ pages/impressum.vue
@@ -8,13 +8,3 @@
     </div>
   </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-
-export default Vue.extend({})
-</script>
-
-<style lang="less" scoped>
-
-</style>
